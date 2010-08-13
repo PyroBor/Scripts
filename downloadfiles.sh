66073f5bda7a956cd81a4b7a9a7529465507f544
Bor Kraljič <pyrobor@ver.si>
2010-08-13 09:42:02 +0200

    downloadfiles.sh: old and useless

--- downloadfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-download_url=""
-#elinks --dump "http://www.sub-titles.net/ppodnapisi/search?tbsl=3&asdp=1&sK=$seria&sJ=$lang&sTS=$season&sTE=$episode#" |grep -m1 -E -o  "http://www.sub-titles.net/ppodnapisi/download/i/.*"
-# i need to download *.zip *.doc *.ppt
-# from http://www.farma-drustvo.si/gradivo.php?b=gradivo_p%2FAnalizna+kemija{%2FIZPITI/%2FPREDAVANJA/%2FVAJE}
--- h10.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -r -n -t --progress --delete --ignore-existing --size-only /home/bor/music/ /mnt/iriver/Music/
