a241227ff839b62a9c23ecb35d5c4b97cb7985f5
Bor Kraljič <pyrobor@ver.si>
2010-09-09 10:11:05 +0200

    copy_spells_git2grim.sh: upadated header

--- copy_spells_git2grim.sh
+++ copy_spells_git2grim.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
+#---
+## by Bor Kraljič <pyrobor[at]ver[dot]si>
+## Licence is GPL v2 or higher
+##
 ## copy spells from git to grimoire
-
+##
+## TODO
+## - add param to set git_dir &grimoire
+## - maybe remove run_as_root since is only from patch sorcery bug #15421
+## - option to copy from grimoire to git_dir
+#---
 
 . /etc/sorcery/config
 if  [  "$UID"  !=  0  ];  then
