847be948af3b31126f36b6464f623ff79590075a
Bor Kraljič <pyrobor@ver.si>
2011-10-14 12:28:17 +0200

    spells_massinfo.sh: prepare for bugs or wiki...
    It still needs source edit for best resoults :)

--- spells_massinfo.sh
+++ spells_massinfo.sh
@@ -3,7 +3,7 @@
 ## by Bor Kraljič <pyrobor[at]ver[dot]si>
 ## Licence is GPL v2 or higher
 ##
-## Prepares info of spell for wiki
+## Prepares info of spell for bugs or wiki
 #---
 
 # let's get sorcery goodies
@@ -11,11 +11,32 @@
 
 ## mass info from spells
 
+destination=$1
+
 for spell in "$@"; do
   (
    codex_set_current_spell_by_name $spell
-   ########## edit this line
-   message "|| [[http://www.google.com/search?q=$spell|$spell]] || $VERSION || $WEB_SITE ||  ||"
-   #########
+   if [[ $destination == "bug" ]]; then
+    ########## edit this part
+    message "================================"
+    message "$SPELL linux 3.0 support check"
+    echo ""
+    message "Spell *$SPELL* should be checked if it still works properly with linux 3.0.*."
+    echo ""
+    message "h3. Spell info:"
+    echo ""
+    message "*SPELL*: $SPELL"
+    message "*VERSION*:$VERSION*"
+    message "*SHORT*: $SHORT"
+    message "*WEB_SITE*: $WEB_SITE"
+    message "*current spell files*: source:$SECTION/$SPELL"
+    #########
+   elif [[ $destination == "wiki" ]]; then
+    ########## edit this part
+    echo "||$SPELL||$VERSION||$WEB_SITE||source:$SECTION/$SPELL||"
+    ##########
+   else
+    echo "$SPELL - $VERSION"
+   fi
   )
 done
