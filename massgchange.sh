6beb225ce3fb4d4943281a06b292fa1ee7dc177c
Bor Kraljič <pyrobor@ver.si>
2011-10-22 10:59:06 +0200

    massgchange: cosmetic changes

--- massgchange.sh
+++ massgchange.sh
@@ -13,7 +13,7 @@ for details in $details_w_sigfile; do
   section=$(echo $details |cut -f2 -d/)
   spell=$(echo $details |cut -f3 -d/)
   sigignore=$(grep -r -E "^ *SOURCE2_IGNORE=" $details)
-  if [[ $sigignore == "" ]]; then
+  if [[ ! $sigignore ]]; then
 #      sed -i -e "s/^ *SOURCE2=.*[(sig)|(asc)|(sign)]$/&\n  SOURCE2_IGNORE=signature/" $details
 #      sed -i "1 s%^.*$%2010-06-15 Bor Kraljič <pyrobor@ver.si>\n\t* DETAILS: added SOURCE2_IGNORE for signature\n\n&%" ./$section/$spell/HISTORY
 #     echo "\t* DETAILS: added SOURCE2_IGNORE for signature"
